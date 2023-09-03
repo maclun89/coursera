2d644096a18fb1d6b548afceeab26f19318ff3bc
Martin Culba <mculba@crowlyn.com>
2023-09-03 11:17:43 +0200

    Remove Old trash

--- LittleLemonBackEndDeveloperCapstonePeerReview/LittleLemon/settings.py
+++ LittleLemonBackEndDeveloperCapstonePeerReview/LittleLemon/settings.py
@@ -79,7 +79,18 @@ WSGI_APPLICATION = "LittleLemon.wsgi.application"
 
 
 DATABASES = {
-    "default": {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    },
+    "default_local": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
